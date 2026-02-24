be8999021e4ad213f2cf63718e9efc7278a9f284
eeelkina <eeelkina@edu.hse.ru>
2026-02-24 10:24:26 +0300

    Add Renderer class

---
+++ include/Renderer.h
@@ -0,0 +1,14 @@
+//
+// Created by Катя Елкина on 24.02.2026.
+//
+
+#ifndef SNAKE_RENDERER_H
+#define SNAKE_RENDERER_H
+
+
+class Renderer
+{
+};
+
+
+#endif //SNAKE_RENDERER_H
---
+++ src/Renderer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Катя Елкина on 24.02.2026.
+//
+
+#include "../include/Renderer.h"
