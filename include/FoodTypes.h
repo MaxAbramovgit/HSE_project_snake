007491c2026a7baffe82a18321f9c47497e45e38
sophie <sophie@MacBook-Air-sophie.local>
2026-02-28 10:55:00 +0300

    Foodtypes types of food

--- include/FoodTypes.h
+++ include/FoodTypes.h
@@ -1,5 +1,16 @@
 #pragma once
+#include <memory>
+#include "Board.h"
+#include "GameEntity.h"
 
-class FoodTypes {
-};
-
+class FoodTypes : GameEntity {
+public:
+    enum class Type {
+        BANANA,
+        HAMBURGER,
+        BOMB,
+        POISON_APPLE
+    };
+    Type food_type;
+    FoodTypes(int x, int y, Type R);
+    int GetPoints();
--- src/FoodTypes.cpp
+++ src/FoodTypes.cpp
@@ -3,3 +3,17 @@
 //
 
 #include "../include/FoodTypes.h"
+FoodTypes::FoodTypes(int x, int y, Type R): GameEntity(x, y), food_type(R){}
+
+int FoodTypes::GetPoints() {
+    if (food_type == Type::BANANA) {
+        return 1;
+    }
+    if (food_type == Type::HAMBURGER) {
+        return 2;
+    }
+    if (food_type == Type::POISON_APPLE) {
+        return -1;
+    }
+    return 0;
+}
