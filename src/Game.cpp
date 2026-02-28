12dab68c4f5e89c8b08f6974dc31c14a488c887b
eeelkina <eeelkina@edu.hse.ru>
2026-02-28 10:56:13 +0300

    i ve made some change

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,3 +1,70 @@
-#include "../include/Game.h"
 #include <iostream>
 #include "Game.h"
+
+
+class Game
+{
+    Game::Game(int width, int height) : board(width, height), foodgenerator(width, height), renderer(board), running(true) { }
+
+    void Game::handleInput()
+    {
+        char input;
+        std::cin >> input;
+
+        auto& snake = board.getSnake();
+
+        if (input == "w" || input == "W")
+        {
+            snake.setDirection(Direction::UP);
+        }
+
+        if (input == "s" || input == "S")
+        {
+            snake.setDirection(Direction::DOWN);
+        }
+
+        if (input == "a" || input == "A")
+        {
+            snake.setDirection(Direction::LEFT);
+        }
+
+        if (input == "d" || input == "D")
+        {
+            snake.setDirection(Direction::RIGHT);
+        }
+
+        if (input == "q" || input == "Q")
+        {
+            running = false;
+        }
+    }
+
+    void Game::update()
+    {
+        board.update();
+
+        if (board.getFood() == nullptr && !(board.isGameOver()))
+        {
+            auto addFood = foodgenerator.generateFoodNotOnSnake(board.getSnake());
+            board.setFood(std::move(addFood));
+        }
+    }
+
+    void Game::render() const
+    {
+        renderer.render();
+    }
+
+    bool Game::isRunning()
+    {
+        return (running && !(board.isGameOver));
+    }
+
+    void Game::run()
+    {
+        while(isRunning)
+        {
+
+        }
+    }
+};
--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -1,2 +1,63 @@
-#include "../include/Renderer.h"
 #include <iostream>
+#include "Renderer.h"
+#include "Board.h"
+#include "Food.h"
+#include "FoodType.h"
+
+
+class Renderer
+{
+    Renderer::Renderer(const Board& newboard) : board(newboard) { }
+
+    void Renderer::clearScreen()
+    {
+        system("clear");
+    }
+
+    void Renderer::render() const
+    {
+        clearScreen();
+
+        std::vector<std::vector<char>> field(height, std::vector<char>(width, '.'));
+
+        for (int i = 0; i < board.getSnake().getBody().size(); ++i)
+        {
+            int currentX = board.getSnake().getBody()[i]->getX;
+            int currentY = board.getSnake().getBody()[i]->getY;
+
+            field[currentY][currentX] = 'Z'; //means that snake is in that place
+        }
+
+        Food* food = board.getFood();
+        if (board.getFood() != nullptr)
+        {
+            int currentX = board.getFood()->getX;
+            int currentY = board.getFood()->getY;
+
+            if (dynamic_cast<POISONAPPLE*>(food))
+            {
+                field[currentX][currentY] = 'P';
+            }
+
+            if (dynamic_cast<BANANA*>(food))
+            {
+                field[currentX][currentY] = 'B';
+            }
+
+            if (dynamic_cast<HAMBURGER*>(food))
+            {
+                field[currentX][currentY] = 'H';
+            }
+
+            if (dynamic_cast<BOMB*>(food))
+            {
+                field[currentX][currentY] = 'B';
+            }
+        }
+    }
+
+    void Renderer::renderGameOver() const
+    {
+
+    }
+};
