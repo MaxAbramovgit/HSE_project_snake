894f695ddb0136debf1d310d135ca5ab073b754b
Anastasia <anastasia@MacBook-Air-Anastasia.local>
2026-03-01 14:54:44 +0300

    editing Segment.cpp - deleting division into different segments (normal and ghost)

--- src/Segment.cpp
+++ src/Segment.cpp
@@ -1,11 +1,7 @@
 #include "../include/Segment.h"
 
-Segment::Segment(int startX, int startY, SegmentType t)
-    : GameEntity(startX, startY), type(t) {}
-
-SegmentType Segment::getType() const { return type; }
-void Segment::setType(SegmentType t) { type = t; }
+Segment::Segment(int startX, int startY) : GameEntity(startX, startY) {}
 
 char Segment::getSymbol() const {
-    return (type == SegmentType::GHOST) ? '~' : '#';
+    return 'O';
 }
