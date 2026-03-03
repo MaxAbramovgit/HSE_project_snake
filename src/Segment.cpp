f4731e799669d7914b71bc209c12feb0e830e943
nastya-sla <aaslanevskaya@edu.hse.ru>
2026-03-03 13:41:25 +0300

    I HAVE PROBLEMS WITH COMMITS

--- src/Segment.cpp
+++ src/Segment.cpp
@@ -1,4 +1,4 @@
 #include "../include/Segment.h"
 
 int a = 0;
-Segment::Segment(int startX, int tartY) : GameEntity(startX, startY) {}
+Segment::Segment(int startX, int startY) : GameEntity(startX, startY) {}
