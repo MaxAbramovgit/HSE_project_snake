a8fd7d486c7ff81feab3be0944fb0b8d1bf74fac
Mania-s <slostreykova@edu.hse.ru>
2026-03-07 11:12:22 +0300

    i rewrote FoodTypes.cpp using switch

--- src/FoodTypes.cpp
+++ src/FoodTypes.cpp
@@ -1,16 +1,18 @@
 #include "../include/FoodTypes.h"
 #include "../include/GameEntity.h"
+#include "../include/Board.h"
 FoodTypes::FoodTypes(int x, int y, Type R) : Food(x, y), food_type(R){}
 
 int FoodTypes::GetPoints() const {
-    if (food_type == Type::BANANA) {
-        return 1;
-    }
-    if (food_type == Type::HAMBURGER) {
-        return 2;
-    }
-    if (food_type == Type::POISON_APPLE) {
-        return -1;
+    switch (food_type) {
+        case Type::BANANA:
+            return 1;
+        case Type::HAMBURGER:
+            return 2;
+        case Type::POISON_APPLE:
+            return -1;
+        case Type::BOMB:
+            return -3;
     }
     return 0;
 };
