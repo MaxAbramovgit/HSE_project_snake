1cab03932fee7eb4fd0b7660975feb440dd28d24
MaxAbramovgit <mvabramov@edu.hse.ru>
2026-03-07 11:43:08 +0300

    FoodGenerator.cpp\

--- src/FoodGenerator.cpp
+++ src/FoodGenerator.cpp
@@ -1,6 +1,10 @@
-#include "../include/FoodGenerator.h"
+#include "FoodGenerator.h"
+#include "FoodTypes.h"
+#include <memory>
+#include <cstdlib>
+#include <ctime>
 
-FoodGenerator::FoodGenerator(int x, int y) : GameField(x, y) {
+FoodGenerator::FoodGenerator(int w, int h) : GameField(w, h) {
     static bool seeded = false;
     if (!seeded) {
         std::srand(static_cast<unsigned>(std::time(nullptr)));
@@ -9,17 +13,30 @@ FoodGenerator::FoodGenerator(int x, int y) : GameField(x, y) {
 }
 
 std::unique_ptr<Food> FoodGenerator::generateRandomFood() {
-    int x = std::rand() % getWidth();
-    int y = std::rand() % getHeight();
-    int type = std::rand() % 4;  // 0,1,2,3
+    int x = std::rand() % GetWidth();
+    int y = std::rand() % GetHeight();
+    int type = std::rand() % 4;
 
+    FoodTypes::Type foodType;
     switch (type) {
-        case 0: return std::make_unique<SpeedFood>(x, y);
-        case 1: return std::make_unique<SlowFood>(x, y);
-        case 2: return std::make_unique<GhostFood>(x, y);
-        case 3: return std::make_unique<PoisonFood>(x, y);
-        default: return std::make_unique<SpeedFood>(x, y);
+        case 0:
+            foodType = FoodTypes::Type::BANANA;
+            break;
+        case 1:
+            foodType = FoodTypes::Type::HAMBURGER;
+            break;
+        case 2:
+            foodType = FoodTypes::Type::POISON_APPLE;
+            break;
+        case 3:
+            foodType = FoodTypes::Type::BOMB;
+            break;
+        default:
+            foodType = FoodTypes::Type::BANANA;
+            break;
     }
+
+    return std::make_unique<FoodTypes>(x, y, foodType);
 }
 
 std::unique_ptr<Food> FoodGenerator::generateFoodNotOnSnake(const Snake& snake) {
