2094b1908f8e10df2a3b52ee3bdc02c5406b51c0
nastya-sla <aaslanevskaya@edu.hse.ru>
2026-03-09 23:59:39 +0300

    I have priblems with commits - there is no anything before 3rd of March, they were marked gray, so I decided to re-commit my work

--- include/Segment.h
+++ include/Segment.h
@@ -1,9 +1,1 @@
-#pragma once
 
-#include "GameEntity.h"
-
-class Segment : public GameEntity {
-public:
-    Segment(int startX, int startY);
-    char getSymbol() const override;
-};
--- src/Segment.cpp
+++
@@ -1,7 +0,0 @@
-#include "../include/Segment.h"
-
-Segment::Segment(int startX, int startY) : GameEntity(startX, startY) {}
-
-char Segment::getSymbol() const {
-    return 'O';
-}
