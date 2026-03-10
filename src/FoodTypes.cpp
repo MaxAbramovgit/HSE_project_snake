6d7a176534d90eb7fe08ed32049d122f655e375b
Mania-s <slostreykova@edu.hse.ru>
2026-03-10 14:15:51 +0300

    redone the mistake w constexpr

--- src/FoodTypes.cpp
+++ src/FoodTypes.cpp
@@ -5,22 +5,22 @@ FoodTypes::FoodTypes(int newX, int newY, Type R) : Food(newX, newY), food_type(R
 
 int FoodTypes::getPoints()const{
     switch (food_type) {
-        case Type::BANANA:
-            constexpr int
-            BANANA_POINTS = 1;
+        case Type::BANANA: {
+            constexpr int BANANA_POINTS = 1;
             return BANANA_POINTS;
-        case Type::HAMBURGER:
-            constexpr int
-            HAMBURGER_POINTS = 2;
+        }
+        case Type::HAMBURGER: {
+            constexpr int HAMBURGER_POINTS = 2;
             return HAMBURGER_POINTS;
-        case Type::POISON_APPLE:
-            constexpr int
-            POISON_POINTS = -1;
+        }
+        case Type::POISON_APPLE: {
+            constexpr int POISON_POINTS = -1;
             return POISON_POINTS;
-        case Type::BOMB:
-            constexpr int
-            BOMB_POINTS = -3;
+        }
+        case Type::BOMB: {
+            constexpr int BOMB_POINTS = -3;
             return BOMB_POINTS;
+        }
     }
     return 0;
 };
