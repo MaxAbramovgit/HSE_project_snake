59b83b65339df0bc328bbd8434f8901859551987
nastya-sla <aaslanevskaya@edu.hse.ru>
2026-03-12 17:37:24 +0300

    made my own testing file for game entity

--- tests/test_game_entity.cpp
+++ tests/test_game_entity.cpp
@@ -1,3 +1,31 @@
 //
 // Created by Anastasia on 12.03.2026.
 //
+
+#include <gtest/gtest.h>
+#include "../include/GameEntity.h"
+
+TEST(GameEntityTest, ConstructorAndGetters) {
+    GameEntity entity(5, 10);
+    EXPECT_EQ(entity.getX(), 5);
+    EXPECT_EQ(entity.getY(), 10);
+}
+
+TEST(GameEntityTest, SetPosition) {
+    GameEntity entity(5, 10);
+    entity.setPosition(7, 12);
+    EXPECT_EQ(entity.getX(), 7);
+    EXPECT_EQ(entity.getY(), 12);
+}
+
+TEST(GameEntityTest, DefaultSymbol) {
+    GameEntity entity(1, 1);
+    EXPECT_EQ(entity.getSymbol(), '?');
+}
+
+TEST(GameEntityTest, VirtualDestructor) {
+    GameEntity* entity = new GameEntity(3, 4);
+    EXPECT_EQ(entity->getX(), 3);
+    EXPECT_EQ(entity->getY(), 4);
+    delete entity;
+}
